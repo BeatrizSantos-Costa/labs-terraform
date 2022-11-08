d844a9f297f205e0519eaec9629f741f2307261b
darede <admin@bancomaxima.com.br>
2022-11-08 14:30:36 -0300

    ajustes no source

--- modulos/terraform-modulos/modules.tf
+++ modulos/terraform-modulos/modules.tf
@@ -1,11 +1,11 @@
 module "kms" {
-  source = "./kms"
+  source = "./terraform-modulos/kms"
 
   deletion_window_in_days = var.deletion_window_in_days
 }
 
 module "s3" {
-  source = "./s3"
+  source = "./terraform-modulos/s3"
 
   bucket_name       = var.bucket_name
   definicao_acl     = var.definicao_acl
