16378696fbf58660a8b7a2f3f940b6adabe65f53
darede <admin@bancomaxima.com.br>
2022-11-14 10:45:11 -0300

    Add variables dos modulos

--- terraform-modulos/variables.tf
+++ terraform-modulos/variables.tf
@@ -1,12 +1,24 @@
-variable "bucket_name" {
-  default = "terraform-outputs-poc-com-estag"
+variable "name_kms_alias" {
+  default = ""
+  description = "Alias da chave KMS"
 }
 
-variable "definicao_acl" {
-  default = "private"
+variable "instance_type" {
+  default = ""
+  description = "Tipo da Intancia EC2"
 }
 
+variable "volume_type" {
+  default = ""
+  description = "Tipo do Volume EBS"
+}
 
-variable "deletion_window_in_days" {
-  default = "10"
+variable "volume_size_ebs1"{
+  default = ""
+  description = "Tamanho do volume EBS"
+}
+
+variable "volume_size_ebs2" {
+  default = ""
+  description = "Tamanho do volume EBS"
 }
