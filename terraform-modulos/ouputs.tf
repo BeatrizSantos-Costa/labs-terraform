024b39cd9ed668492a1f250da2fa356f2e6a4ed5
darede <admin@bancomaxima.com.br>
2022-11-14 10:43:02 -0300

    Deletando recursos de redes

--- terraform-modulos/ouputs.tf
+++
@@ -1,4 +0,0 @@
-output "name" {
-  description = "The arn of the key alias"
-  value       = module.name.key_alias_arn
-}
--- terraform-modulos/subnet/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "POC-Terraform"
-  }
-}
