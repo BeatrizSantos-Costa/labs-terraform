9b23c679dccd58f9ce7b047b1b6533c9e4a6ac51
darede <admin@bancomaxima.com.br>
2022-11-14 10:44:28 -0300

    ajustando os modulos

--- terraform-modulos/modules.tf
+++ terraform-modulos/modules.tf
@@ -1,2 +1,13 @@
-module "name" {}
+module "kms" {
+    source = "./kms"
+    name_kms_alias = var.name_kms_alias
+}
+
+module "ec2" {
+    source = "./ec2"
+    instance_type = var.instance_type
+    volume_type = var.volume_type
+    volume_size_ebs1 = var.volume_size_ebs1
+    volume_size_ebs2 = var.volume_size_ebs2
+}
 
