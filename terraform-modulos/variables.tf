412c31a4e4ef3844cbd0b5f5b896338cf6405992
darede <admin@bancomaxima.com.br>
2022-11-16 10:20:51 -0300

    ajustes no arquivos de variables

--- terraform-modulos/variables.tf
+++ terraform-modulos/variables.tf
@@ -1,25 +1,20 @@
 variable "name_kms_alias" {
-  default = ""
   description = "Alias da chave KMS"
 }
 
 variable "instance_type" {
-  default = ""
   description = "Tipo da Intancia EC2"
 }
 
 variable "volume_type" {
-  default = ""
   description = "Tipo do Volume EBS"
 }
 
 variable "volume_size_ebs1"{
-  default = ""
   description = "Tamanho do volume EBS"
 }
 
 variable "volume_size_ebs2" {
-  default = ""
   description = "Tamanho do volume EBS"
 }
 
