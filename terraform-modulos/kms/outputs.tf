367aa4b6923972c0c6b6924558a168633d3db25d
darede <admin@bancomaxima.com.br>
2022-11-16 10:17:33 -0300

    ajustes de outputs

--- terraform-modulos/kms/outputs.tf
+++ terraform-modulos/kms/outputs.tf
@@ -1,7 +1,7 @@
-output "kms-alias-ebs" {
-  value = aws_kms_alias.name
+output "alias_kms_ebs" {
+  value = aws_kms_alias.kms-alias-ebs.name
 }
 
-output "kms-key-ebs" {
-  value = aws_kms_key.id
+output "key_kms_ebs" {
+  value = aws_kms_key.kms-key-ebs.id
 }
