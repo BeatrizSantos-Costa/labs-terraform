447055d088a53b9274b490caa91005924aaf6648
darede <admin@bancomaxima.com.br>
2022-11-16 10:16:31 -0300

    ajustes de formatação e add kms e dependos

--- terraform-modulos/modules.tf
+++ terraform-modulos/modules.tf
@@ -1,13 +1,15 @@
 module "kms" {
-    source = "./kms"
-    name_kms_alias = var.name_kms_alias
+    source          = "./kms"
+    name_kms_alias  = var.name_kms_alias
 }
 
 module "ec2" {
-    source = "./ec2"
-    instance_type = var.instance_type
-    volume_type = var.volume_type
-    volume_size_ebs1 = var.volume_size_ebs1
-    volume_size_ebs2 = var.volume_size_ebs2
+    source              = "./ec2"
+    instance_type       = var.instance_type
+    volume_type         = var.volume_type
+    volume_size_ebs1    = var.volume_size_ebs1
+    volume_size_ebs2    = var.volume_size_ebs2
+    kms                 = module.kms.key_kms_ebs
+    depends_on          = [module.kms]
 }
 
