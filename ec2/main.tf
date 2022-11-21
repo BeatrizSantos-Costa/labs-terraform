cf2bed3cf08a50ebf5f405a6aa49ed61022c46cc
darede <admin@bancomaxima.com.br>
2022-11-21 14:22:39 -0300

    add terraform ec2

---
+++ ec2/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "instance_t2" {
+  ami           = "ami-09d3b3274b6c5d4aa" #AMI AMAZON LINUX
+  instance_type = var.instance_type
+  
+  ebs_block_device {
+    device_name = "/dev/xvda"
+    volume_size = var.volume_size_ebs
+    volume_type = var.volume_type
+    encrypted   = true
+    kms_key_id  = var.kms
+    tags = {
+      Name = "POC-Terraform"
+    }
+  }
+  tags = {
+    Name = "POC-Terraform"
+  }
+}
+
