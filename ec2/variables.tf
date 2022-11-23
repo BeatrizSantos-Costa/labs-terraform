5e9ddefe4b736a5cb14a9c2495b117177ad219f0
darede <admin@bancomaxima.com.br>
2022-11-23 14:24:15 -0300

    add default var

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,11 +1,14 @@
 variable "instance_type" {
   description = "Tipo da Intancia EC2"
+//  default = "t2.micro"
 }
 
 variable "volume_type" {
   description = "Tipo do Volume EBS"
+ // default = "gp3"
 }
 
 variable "volume_size_ebs"{
   description = "Tamanho do volume EBS"
+ // default = "10"
 }
