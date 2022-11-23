4962c7f42f12818124598e3c21a88a6f56dd20fc
darede <admin@bancomaxima.com.br>
2022-11-23 14:43:57 -0300

    ajustes

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance_t2" {
   ami           = "ami-09d3b3274b6c5d4aa" #AMI AMAZON LINUX
   instance_type = var.instance_type
-  
+
   ebs_block_device {
     device_name = "/dev/xvda"
     volume_size = var.volume_size_ebs
