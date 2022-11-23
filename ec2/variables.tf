912c778c35517380d681b14af794946233e4e2be
darede <admin@bancomaxima.com.br>
2022-11-23 12:07:18 -0300

    add arquivo de variables

---
+++ ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  description = "Tipo da Intancia EC2"
+}
+
+variable "volume_type" {
+  description = "Tipo do Volume EBS"
+}
+
+variable "volume_size_ebs"{
+  description = "Tamanho do volume EBS"
+}
